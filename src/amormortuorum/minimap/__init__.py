017441c46f59504269073ef783a88903f7db3206
chris-provolt <104653677+chris-provolt@users.noreply.github.com>
2025-09-02 09:08:15 -0700

    chore: update src/amormortuorum/minimap/__init__.py (automated)

---
+++ src/amormortuorum/minimap/__init__.py
@@ -0,0 +1,21 @@
+from .icons import (
+    FeatureType,
+    MinimapIcon,
+    MinimapIconDescriptor,
+    MinimapIconRegistry,
+    MinimapIconService,
+    MinimapError,
+    DiscoveryAmbiguityError,
+    UnknownFeatureError,
+)
+
+__all__ = [
+    "FeatureType",
+    "MinimapIcon",
+    "MinimapIconDescriptor",
+    "MinimapIconRegistry",
+    "MinimapIconService",
+    "MinimapError",
+    "DiscoveryAmbiguityError",
+    "UnknownFeatureError",
+]
