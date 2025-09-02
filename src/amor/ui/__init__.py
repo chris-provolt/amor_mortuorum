5cabbb3f4cde0d3a7280eda0e79fc6113ced553e
chris-provolt <104653677+chris-provolt@users.noreply.github.com>
2025-09-02 09:49:39 -0700

    chore: update src/amor/ui/__init__.py (automated)

---
+++ src/amor/ui/__init__.py
@@ -0,0 +1,6 @@
+"""
+UI components for Amor Mortuorum.
+
+For this iteration, we provide a simple text-based Graveyard relics panel
+that listens to relic updates and exposes a renderable text string.
+"""
