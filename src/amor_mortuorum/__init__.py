120ed987bfac5d5993032115846d0f8a4da5ab37
chris-provolt <104653677+chris-provolt@users.noreply.github.com>
2025-09-02 09:17:52 -0700

    chore: update src/amor_mortuorum/__init__.py (automated)

---
+++ src/amor_mortuorum/__init__.py
@@ -0,0 +1,10 @@
+'''
+Amor Mortuorum core package.
+
+This package contains the game domain: characters, items, inventory, and systems.
+'''
+__all__ = [
+    'items',
+    'inventory',
+    'characters',
+]
