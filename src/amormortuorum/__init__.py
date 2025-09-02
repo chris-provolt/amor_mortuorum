c83ce866de70a9947ce64e01c26aa7dd91e734fb
chris-provolt <104653677+chris-provolt@users.noreply.github.com>
2025-09-02 07:39:04 -0700

    chore: update src/amormortuorum/__init__.py (automated)

---
+++ src/amormortuorum/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+
+__all__ = [
+    "__version__",
+]
+
+try:
+    __version__ = version("amormortuorum")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
